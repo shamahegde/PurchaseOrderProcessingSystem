74fc89a1a83d7c46d7110b2e0d30ffc3001e145c
Shama <shama.hegde@gmail.com>
2018-09-30 22:00:44 +0530

    error handled

--- POPS/Program.cs
+++ POPS/Program.cs
@@ -7,46 +7,53 @@ namespace POPS
         public enum AccType { Savings=0,Current};
         static void Main(string[] args)
         {
-            //create savings account
-            createAccount(AccType.Savings,1100,"Shama");
-            //create current account
-            createAccount(AccType.Current, 2000, "Jane");
-            //throw exception if account is opened with bal<1000
-            createAccount(AccType.Savings, 10, "Vani");
+            try
+            {
+                //create savings account
+                createAccount(AccType.Savings, 1100, "Shama");
+                //create current account
+                createAccount(AccType.Current, 2000, "Jane");
+                //throw exception if account is opened with bal<1000
+                createAccount(AccType.Savings, 10, "Vani");
 
-            createAccount(AccType.Savings, 5000, "Philip");
-            //remove account
-            AppData.RemoveAccount(100000000001);
-            //withdraw
-            Account accObjJane = AppData.accountsData[100000000002];
-            accObjJane.withdraw(1000);
-            //withdraw again
-            accObjJane.withdraw( 600);
+                createAccount(AccType.Savings, 5000, "Philip");
+                //remove account
+                AppData.RemoveAccount(100000000001);
+                //withdraw
+                Account accObjJane = AppData.accountsData[100000000002];
+                accObjJane.withdraw(1000);
+                //withdraw again
+                accObjJane.withdraw(600);
 
-            //withdraw from savings account
-            Account accObjShama = AppData.accountsData[100000000001];
-            accObjShama.withdraw(100);
-            accObjShama.withdraw(100);
-            //deposit
-            SavingsAccount accObjPhilip = (SavingsAccount)AppData.accountsData[100000000003];
-            accObjPhilip.deposit( 1000);
+                //withdraw from savings account
+                Account accObjShama = AppData.accountsData[100000000001];
+                accObjShama.withdraw(100);
+                accObjShama.withdraw(100);
+                //deposit
+                SavingsAccount accObjPhilip = (SavingsAccount)AppData.accountsData[100000000003];
+                accObjPhilip.deposit(1000);
 
-            //transfer from Philip to Jane
-            Console.WriteLine("Before Transfer");
-            accObjPhilip.getAccountDetails();
-            accObjJane.getAccountDetails();
-            accObjPhilip.TransferAmount(100000000002, 2000);
-            Console.WriteLine("After Transfer");
-            accObjPhilip.getAccountDetails();
-            accObjJane.getAccountDetails();
+                //transfer from Philip to Jane
+                Console.WriteLine("Before Transfer");
+                accObjPhilip.getAccountDetails();
+                accObjJane.getAccountDetails();
+                accObjPhilip.TransferAmount(100000000002, 2000);
+                Console.WriteLine("After Transfer");
+                accObjPhilip.getAccountDetails();
+                accObjJane.getAccountDetails();
 
 
-            //edit account
-            Console.WriteLine(Environment.NewLine);
-            accObjJane.getAccountDetails();
-            accObjJane.editAccount("Jovie");
-            accObjJane.getAccountDetails();
-            Console.ReadLine();
+                //edit account
+                Console.WriteLine(Environment.NewLine);
+                accObjJane.getAccountDetails();
+                accObjJane.editAccount("Jovie");
+                accObjJane.getAccountDetails();
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public static void createAccount(AccType actype,int bal,string uname)
